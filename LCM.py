d03ad6611eba43638f81b527ed2f8940aae8612e
Imran Javed <96229355+ImranJaved1073@users.noreply.github.com>
2022-10-24 21:33:59 +0500

    Create LCM.py
    This program computes LCM of two numbers.

---
+++ LCM.py
@@ -0,0 +1,17 @@
+number1 = int(input("Enter number 1: ")) 
+while number1 < 0:
+    print("ERROR! Number is invalid.\nEnter a number again.")
+    number1 = int(input("Enter number 1: ")) 
+
+number2 = int(input("Enter number 2: ")) 
+while number2 < 0:
+    print("ERROR! Number is invalid.\nEnter a number again.")
+    number2 = int(input("Enter number 2: ")) 
+
+i=0;   
+for  i in range(1, number2+1):
+   if number1 % i == 0 and number2 % i == 0: 
+      hcf = i
+
+lcm = int((number1 * number2) / hcf)
+print( "LCM of ",number1, " and ", number2," is:", lcm )
